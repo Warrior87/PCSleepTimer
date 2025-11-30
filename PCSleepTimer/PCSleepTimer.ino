01d82cdc22017b5a9618e5ad3ae98fe00d33525d
Warrior87 <buerk1a@cmich.edu>
2025-11-29 21:44:17 -0500

    Update PCSleepTimer.ino
    removed IR capability

--- PCSleepTimer/PCSleepTimer.ino
+++ PCSleepTimer/PCSleepTimer.ino
@@ -1,15 +1,6 @@
 #include <Keyboard.h>
-byte GNDPin = 3;
-byte VCCPin = 2;
-byte dataPin = 4;
-byte sleepSetFlag = 0;
 
 void setup() {
-  pinMode(GNDPin, OUTPUT);
-  digitalWrite(GNDPin, LOW);
-  pinMode(VCCPin, OUTPUT);
-  digitalWrite(VCCPin, HIGH);
-  pinMode(dataPin, INPUT_PULLUP);
   Keyboard.begin();
   delay(10000);
   Keyboard.press(KEY_F1);
@@ -18,19 +9,4 @@ void setup() {
 }
 
 void loop() {
-  if(!sleepSetFlag){
-    if(digitalRead(dataPin) == 0){
-      Keyboard.press(KEY_LEFT_GUI);
-      Keyboard.press('r');
-      Keyboard.releaseAll();
-      delay(1000);
-      Keyboard.print("shutdown -s -t 1800");
-      delay(1000);
-      Keyboard.press(KEY_RETURN);
-      delay(100);
-      Keyboard.releaseAll();
-      delay(100);
-      sleepSetFlag = 1;
-    }
-  }
 }
